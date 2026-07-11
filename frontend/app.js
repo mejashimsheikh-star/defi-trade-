90d54d8b46b4f5b07524faf7d762599fa0bdc91d
mejashimsheikh-star <mejashimsheikh@gmail.com>
2026-07-11 06:44:29 +0600

    Homepage UI with Live BTC Simulation

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,16 @@
-y
+let price = 118500;
+
+function updatePrice(){
+
+    let change = (Math.random() * 1000) - 500;
+
+    price += change;
+
+    document.getElementById("btc").innerHTML =
+        "$" + price.toFixed(2);
+
+}
+
+updatePrice();
+
+setInterval(updatePrice,2000);
