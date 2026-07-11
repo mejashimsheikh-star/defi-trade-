15a4b576a52fdff9ad639f9d24cb223bbe4bc76f
mejashimsheikh-star <mejashimsheikh@gmail.com>
2026-07-11 07:15:29 +0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,3 +7,9 @@ def home():
     return {
         "message": "DeFi Trade API Running"
     } 
+from chat import websocket_endpoint
+
+@app.websocket("/chat")
+async def chat(websocket: WebSocket):
+
+    await websocket_endpoint(websocket)
